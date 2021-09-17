f7c907174d8cd1373292c35f9696edf135bf3d45
LachlanCourt <80299053+LachlanCourt@users.noreply.github.com>
2021-09-17 14:19:08 +1000

    Gen/add input (#2)
    * Add cell
    
    * Generate grid
    
    * Add maze class and move functionality out to tidy up main class
    
    * Formatting

---
+++ src/Maze.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Maze
+{
+
+    ArrayList<ArrayList<Cell>> maze;
+
+    public Maze()
+    {
+        maze = new ArrayList<ArrayList<Cell>>();
+    }
+
+    public void initFromValues(String[] args)
+    {
+        int width = Integer.valueOf(args[0]);
+        int height = Integer.valueOf(args[1]);
+        for (int i = 0; i < height; i++)
+        {
+            maze.add(new ArrayList<Cell>());
+            for (int j = 0; j < width; j++)
+            {
+                maze.get(i).add(new Cell());
+            }
+        }
+    }
+
+    public void generate() {}
+
+    @Override public String toString()
+    {
+        return "Maze!";
+    }
+}
--- src/MazeGenerator.java
+++ src/MazeGenerator.java
@@ -1,8 +1,46 @@
+import java.sql.Array;
+import java.util.ArrayList;
 
 public class MazeGenerator
 {
     public static void main(String[] args)
     {
-        System.out.println("test");
+        if (!validateArgs(args))
+        {
+            System.exit(1);
+        }
+        MazeGenerator MG = new MazeGenerator();
+        MG.run(args);
+    }
+
+    public void run(String[] args)
+    {
+        Maze maze = new Maze();
+        maze.initFromValues(args);
+        maze.generate();
+
+
+        System.out.println(maze);
+    }
+
+    public static boolean validateArgs(String[] args)
+    {
+        String errMess = "Invalid arguments. Usage: <width:int> <height:int> <filename:string>";
+        if (args.length != 3)
+        {
+            System.err.println(errMess);
+            return false;
+        }
+        try
+        {
+            Integer.parseInt(args[0]);
+            Integer.parseInt(args[1]);
+        }
+        catch (Exception e)
+        {
+            System.err.println(errMess);
+            return false;
+        }
+        return true;
     }
 }
